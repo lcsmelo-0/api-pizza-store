cad9c900e07b91c84817c413007a32e31fc5605a
Lucas Melo <luucasmell@gmail.com>
2019-08-11 14:57:27 -0300

    Adicionar dependencias para deploy no heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const department_controller = require('./departmentController');
 const product_controller = require('./productController');
 const app = express();
+const PORT = process.env.PORT || 8000
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
@@ -17,4 +18,4 @@ mongoose.connect(
 app.use('/funcionarios', department_controller);
 app.use('/sabores', product_controller);
 
-app.listen(3000);
+app.listen(PORT);
